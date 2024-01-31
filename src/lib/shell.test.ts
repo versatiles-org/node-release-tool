12743d1ae8c89653b5a2788db86997885f37696d
Michael Kreil <github@michael-kreil.de>
2024-01-31 09:25:14 +0100

    test: shell

---
+++ src/lib/shell.test.ts
@@ -0,0 +1,36 @@
+import { Run } from './shell.js';
+import { jest } from '@jest/globals';
+
+describe('Run', () => {
+	const cwd = new URL('../../', import.meta.url).pathname;
+	const run = Run(cwd);
+
+	it('runs ls', async () => {
+		const ls = await run('ls -1');
+		expect(ls).toMatchObject({ code: 0, signal: null, stderr: '' });
+		expect(ls.stdout).toContain('package.json\n');
+	});
+
+	it('returns stdout', async () => {
+		expect(await run.stdout('ls -1')).toContain('package.json\n');
+	});
+
+	it('returns stderr', async () => {
+		expect(await run.stderr('git status -h', false)).toContain('usage: git status');
+	});
+
+	it('returns ok', async () => {
+		expect(await run.ok('ls -1')).toBe(true);
+	});
+
+	it('returns not ok', async () => {
+		expect(await run.ok('exit 1')).toBe(false);
+	});
+
+	it('fails on exit 1', async () => {
+		const mockError = jest.spyOn(console, 'error');
+		mockError.mockImplementationOnce(() => { });
+		await expect(run('exit 1')).rejects.toEqual({ code: 1, signal: null, stderr: '', stdout: '' });
+		mockError.mockRestore();
+	});
+})
