4fd0cd214f3ee1825fd6918eee32d34d30cdb545
Michael Kreil <github@michael-kreil.de>
2025-11-18 16:48:07 +0100

    fix(tests): add testTimeout to configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		include: ['src/**/*.test.ts']
+		include: ['src/**/*.test.ts'],
+		testTimeout: 30_000,
 	},
 });
